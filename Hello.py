94d1cf26abef930d36cb964499e14bd6e13bded4
StockCurves <147582495+StockCurves@users.noreply.github.com>
2023-10-18 05:16:49 +0000

    first try

--- Hello.py
+++ Hello.py
@@ -15,8 +15,67 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
+import matplotlib.pyplot as plt
+import gpxpy
+import geopy
+
+import matplotlib.font_manager as mpfm
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+# Set Font
+
+
+# mpfm.addfont('./font/SimHei.ttf')
+# plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+# mpfm.fontManager.addfont('./font/NotoSansTC-Regular.ttf')
+# mpfm.fontManager.addfont('./font/Microsoft-JhengHei.ttf')
+# plt.rcParams['font.sans-serif'] = ['NotoSansTC-Regular', 'Microsoft-JhengHei'] 
+# plt.rcParams['axes.unicode_minus'] = False
+# rcParams["font.size"]= 20
+# matplotlib.rc('font', family='SimHei')
+
 LOGGER = get_logger(__name__)
 
+def get_elevation(gpx):
+    import math
+    from geopy import distance 
+
+    yk_lat   = []
+    yk_lon   = []
+    yk_ele0  = [] # gpx elevation
+    yk_ele1  = [] # SRTM elevation from NASA
+    dis_flat = [] # spherical distance
+    dis_eucl = [] # euclidian /b surface distance
+
+    for segment in gpx.tracks[0].segments:
+        for p in segment.points:     
+            lat = p.latitude
+            lon = p.longitude
+            yk_lat.append(lat)
+            yk_lon.append(p.longitude)
+            yk_ele0.append(p.elevation)
+            
+    dis_sum_flat = []
+    dis_sum_eucl = []
+    s = 0
+    s1 = 0
+
+    # point-to-point distance
+    for i, x in enumerate(yk_lat[1:], 1):
+        p1 = (yk_lat[i-1], yk_lon[i-1])
+        p2 = (yk_lat[i], yk_lon[i])    
+        dis = distance.distance(p1, p2).km
+        dis_flat.append( dis )
+        dis_eucl.append( math.sqrt(dis**2 + (yk_ele0[i]/1000 - yk_ele0[i-1]/1000)**2) )
+
+    # cumulated sum    
+    for i, d in enumerate(dis_flat):    
+        s += dis_flat[i]
+        s1 += dis_eucl[i]
+        dis_sum_flat.append(s)
+        dis_sum_eucl.append(s1)
+
+    return [dis_sum_flat, yk_ele0[1:]]
 
 def run():
     st.set_page_config(
@@ -25,27 +84,78 @@ def run():
     )
 
     st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
+    # st.sidebar.success("Select a demo above.")
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+          test
+        """
     )
+    # st.markdown(
+    #     """
+    #     <style>
+    #     @font-face {
+    #       font-family: 'Tangerine';
+    #       font-style: normal;
+    #       font-weight: 12;
+    #       src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
+    #       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    #     }
+    #         html, body, [class*="css"]  {
+    #         font-family: 'Tangerine';
+    #         font-size: 48px;
+    #         }
+    #         </style>
+    #         """,
+    #             unsafe_allow_html=True,
+    # )
+
+
+    # Create a list to store uploaded files
+    uploaded_files = []
+    uploaded_files = st.file_uploader("Upload files", type=["gpx"], accept_multiple_files=True)
+
+    # Display uploaded files and provide a delete option
+    if uploaded_files:
+        st.header("Uploaded Files:")
+        for file in uploaded_files:
+            file_name = file.name
+            # Display file name
+            st.write(file_name)
+            # Create a button to delete the file
+            if st.button(f"Delete {file_name}"):
+                uploaded_files.remove(file)
+                st.success(f"{file_name} has been deleted.")
+                # Optional: You can also delete the file from the server if needed.
+                # os.remove(file_name)
+
+    # Display the uploaded files
+
+    st.subheader("Maps of the Elvation:")
+    fig, d1 = plt.subplots(1,1)
+    fig.set_figheight(12)
+    fig.set_figwidth(15)
+
+    for fn in uploaded_files:
+        # st.write(fn.name)
+        fn = fn.name
+        with open(fn) as f:
+            gpx = gpxpy.parse(f)       
+        [d, e0] = get_elevation(gpx)
+        d1.plot(d, e0, lw=5, label = fn.split('.')[0])    
+        #d1.plot(d, e1, lw=1, color='k', ls = ':' )   
+
+    d1.set_xlabel("距離 (KM)")
+    d1.set_ylabel("海跋 (M)")
+    # d1.legend(prop='PingFang HK');
+    d1.legend();
+    d1.grid()        
+
+    st.pyplot(fig)        
+
+
+        
 
 
 if __name__ == "__main__":
     run()
+
